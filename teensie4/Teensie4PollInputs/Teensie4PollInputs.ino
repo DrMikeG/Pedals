55672e2df829de6c6fb2ab762bb304dea0439b43
DrMikeG <Mike@admins-iMac.local>
2020-10-22 22:21:05 +0100

    Removed volocity code, upped volume, put wait to allow calibration before making any noise.

--- teensie4/Teensie4PollInputs/Teensie4PollInputs.ino
+++ teensie4/Teensie4PollInputs/Teensie4PollInputs.ino
@@ -5,11 +5,10 @@ const int channel = 1;
 
 // Skip A11 because I only need 13 and I burnt the pad off Pin 11...
 int pinsToPoll[13] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A13, A12, A10};
-int pinToPollMax = 13;
+constexpr int pinToPollMax = 13;
 
 int led = 13 ; // LED on arduino
 int analogVal; // analog readings
-
 
 // Triggering variables & consts
 int lowWaterMark[13] = {700,700,700,700,700, 700,700,700,700,700, 700,700,700};
@@ -21,11 +20,6 @@ constexpr int triggerCalibrationThreshold = 40;
 // Average variables & consts
 int_fast16_t headings[13] = {0,0,0,0,0, 0,0,0,0,0, 0,0,0};
 constexpr int_fast16_t fractional_bits = 1;
-
-// Veclocity variables & consts
-int historyOfValues[5] = {0,0,0,0,0};
-constexpr int_fast16_t nHistoryValues = 5;
-constexpr double nMagicConstForVelocity = 300.0;
 
 // Note variables
 int keyVelocity[13] = {0,0,0,0,0, 0,0,0,0,0, 0,0,0} ;
@@ -53,65 +47,12 @@ void setup() {
   MIDI.begin();
 }
 
-int_fast16_t shiftHistoryValues(int_fast16_t newestValue)
-{
-  for (int i=0; i < (nHistoryValues-1); i++)
-  {
-    historyOfValues[i] = historyOfValues[i+1]; 
-  }
-  historyOfValues[nHistoryValues-1] = newestValue;
-  return historyOfValues[0] - newestValue;
-}
-
-int velocityFromShift( int_fast16_t lowWater, int_fast16_t highWater, int_fast16_t deltaInN)
-{
-  // velocity 0 to 127
-
-  // Range is highWater - lowWater
-  double range = (highWater - lowWater);
-  // Trigger is 10% range higher than lowWater
-  //int trigger = lowWater + ( 0.1 * (range) );
-
-  // Instant drop delta would be 1.0*range
-  // Slow drop delta seems to be about 0.2*range
-
-  int velocity = 0;
-  if (deltaInN > 0 && range > 0)
-  { 
-    if (deltaInN > range)
-    { 
-       deltaInN = range;
-    }
-    double deltaF = 1.0*deltaInN;
-    
-    velocity = (int)((deltaF /range) * nMagicConstForVelocity);
-  }
-
-  // Clamp
-  if (velocity < 0)
-  {
-    velocity = 0; 
-  }
-  if (velocity > 127)
-  {
-   velocity = 127; 
-  }
-  
-  return velocity;
-}
+constexpr int waitBeforeMakingNoise = 50*pinToPollMax;
+int nWaited = 0;
 
 void loop() {
-  
-  /*int note;
-  for (note=10; note <= 127; note++) {
-    MIDI.sendNoteOn(note, 100, channel);
-    delay(200);
-    MIDI.sendNoteOff(note, 100, channel);
-  }
-  delay(2000);*/
 
    // Read the analog interface
-  
   for (int i=0; i < pinToPollMax; i++)
   {
     
@@ -121,10 +62,6 @@ void loop() {
       {
         lowWaterMark[i] = analogVal;
       }
-      /*else
-      {
-        lowWaterMark[i]+= ((analogVal-lowWaterMark[i])*0.1);
-      }*/
       
       if (analogVal > highWaterMark[i])
       {
@@ -141,20 +78,12 @@ void loop() {
 
     bool displayOn = true;//(i == 12);
     
-    //Serial.print(analogVal); // print analog value
     if (displayOn)
     {
 
-      //Serial.print(lowWaterMark[i]); // print analog value
-      //Serial.print(",");              //seperator
-      //Serial.print(highWaterMark[i]); // print analog value
-      //Serial.print(",");              //seperator
-      //Serial.print(triggerWaterMark[i]); // print analog value
-      //Serial.print(",");              //seperator
       int_fast16_t avg = average_heading(i,analogVal);
 
-      bool printD =  (displayOn);
-      if (printD)
+      if (displayOn)
       {
         Serial.print(avg); // print analog value
         Serial.print(",");              //seperator
@@ -171,21 +100,20 @@ void loop() {
           Serial.print(",");              //seperator
         }
       }
-     
-      
-      int_fast16_t shift = shiftHistoryValues(avg);
-      int velocity = velocityFromShift(lowWaterMark[i],highWaterMark[i],shift); 
-      //Serial.print( shift );
-      //Serial.print(",");              //seperator
-      //Serial.print( velocity);
 
+
+      // Don't make a noise for the first 
+      if (nWaited < waitBeforeMakingNoise)
+      {
+        nWaited++;
+        continue;
+      }
       
       if ( avg < triggerWaterMark[i] && triggerCalibrated[i])
       {
         if ( keyVelocity[i] == 0 )
         {
-          //keyVelocity[i] = velocity;
-          keyVelocity[i] = 100;
+          keyVelocity[i] = 120;
           MIDI.sendNoteOn(keyAssignment[i], keyVelocity[i], channel);
           digitalWrite (led, HIGH);
         }
@@ -199,10 +127,8 @@ void loop() {
       }
               
     }
-   // Serial.print('\n');
-     
-    //Serial.print(",");              //seperator
+   
   }// end for for each pin
- Serial.print('\n');        
+  Serial.print('\n');        
   delay(20);
 }
